289735ad73fd4272d21aab0fa935fde61bb2de52
Josh Black <joshblack@us.ibm.com>
2017-05-26 21:52:03 -0500

    feat: add multi-bundle support for rollup

---
+++ scripts/rollup/plugins/sizes-plugin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const gzip = require('gzip-size');
+
+module.exports = function sizes(options) {
+  return {
+    ongenerate(bundle, obj) {
+      const size = Buffer.byteLength(obj.code);
+      const gzipSize = gzip.sync(obj.code);
+
+      options.getSize(size, gzipSize);
+    },
+  };
+};
